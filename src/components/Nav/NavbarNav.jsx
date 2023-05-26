3e7b57759bb54f543512d7c897c0839265a44e53
Sadim Mali <sadimmalakar77@gmail.com>
2023-05-26 17:42:58 +0545

    Refactor NavbarNav component

--- src/components/Nav/NavbarNav.jsx
+++ src/components/Nav/NavbarNav.jsx
@@ -5,17 +5,17 @@ import logoImg from '../../assets/img/logo/campfitLogo.png'
 
 function NavbarNav() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar variant='dark' expand="lg">
       <Container>
         <Navbar.Brand href="#home"><img src={logoImg} alt="campfit-logo" style={{width: "4rem"}}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Service</Nav.Link>
-            <Nav.Link href="#link">Gallery</Nav.Link>
-            <Nav.Link href="#link">Community</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/service">Service</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/community">Community</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
