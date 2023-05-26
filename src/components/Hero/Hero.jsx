631bb257ea16098e2fe2c7fdae5e76e66a908631
Sadim Mali <sadimmalakar77@gmail.com>
2023-05-26 17:42:01 +0545

    Refactor Hero component : Add Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,24 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import HeroImg from "../../assets/img/hero/heroImgg.png"
+
 
 function Hero(){
     return(
         <>
             <section id="hero" className="hero-section">
-              <h1>Hero section</h1>
+            <Container>
+            <Row>
+                <Col md={6}>
+                <h1 className='text-uppercase font-head'>build your body into a <span className="primary-text-color">healthy</span> and <span className="primary-text-color">strong body</span>.</h1>
+                <p>Sport is part of heath, so be diligent in exercising so that the body becomes strongerr and healthier to improve health and keep away from injury</p>
+                </Col>
+                <Col md={6} className="text-center">
+                    <img src={HeroImg} alt="hero-img" />
+                </Col>
+            </Row>
+            </Container>
             </section>
         </>
     )
