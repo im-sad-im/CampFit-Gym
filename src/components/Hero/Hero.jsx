a92f598db3e45eeda908dc11484b95d2ef34c2b1
Sadim Mali <sadimmalakar77@gmail.com>
2023-05-27 08:16:39 +0545

    Add play icon to mybtn1, style hero section button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import HeroImg from "../../assets/img/hero/heroImgg.png"
 import  Button  from "react-bootstrap/Button"
+import {BsFillPlayCircleFill} from "react-icons/bs";
 
 
 function Hero(){
@@ -15,8 +16,9 @@ function Hero(){
                 <h1 className='text-uppercase font-head'>build your body into a <span className="primary-text-color">healthy</span> and <span className="primary-text-color">strong body</span>.</h1>
                 <p>Sport is part of heath, so be diligent in exercising so that the body becomes strongerr and healthier to improve health and keep away from injury</p>
                 <div className="d-flex gap-2">
-                <Button className="btn-main">Join Member</Button>
-                <Button variant="dark" style={{borderRadius : "1.5rem"}}>Watch Training</Button>
+                <Button className="btn-main my-btn">Join Member</Button>
+                <Button variant="dark" className="my-btn my-btn1"> 
+                 Watch Training <BsFillPlayCircleFill className="play-icon" style={{fontSize: "1.25rem"}} /></Button>
                 </div>
                 </Col>
                 <Col lg={6} className="text-center">
