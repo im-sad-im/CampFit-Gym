1717988936aebedb61d34fb803548547cec99ee0
Sadim Mali <sadimmalakar77@gmail.com>
2023-05-28 18:16:01 +0545

    Add support component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // import NavbarNav from "../components/Nav/NavbarNav";
 import Hero from "../components/Hero/Hero";
+import Support from "../components/Support/Support";
 
 function Home(){
     return(
         <>
         {/* <NavbarNav */}
         <Hero />
+        <Support />
         </>
     )
 }
