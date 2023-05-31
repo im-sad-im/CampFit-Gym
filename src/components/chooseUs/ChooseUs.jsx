745daf57114f08e9d06c26c09c1aed650057ea0d
Sadim Mali <sadimmalakar77@gmail.com>
2023-05-31 22:13:08 +0545

    Add ChooseUs component

---
+++ src/components/chooseUs/ChooseUs.jsx
@@ -0,0 +1,24 @@
+
+function ChooseUs(){
+    return(
+    <>
+        <section id="chooseUs">
+        <div className="container d-flex gap-4">
+                    <div className="choose-us-content">
+                        <h1 className="text-uppercase">what makes you <span className="primary-text-color">sure</span> to choose us?</h1>
+                        <ol>
+                            <li>Campfit provides free boxing arena and pool facitlites for those who have premium members</li>
+                            <li>There is a cold and comfortable resting rooom for members</li>
+                            <li>Free overseas vacation tickets for members who subscribe for more than 6 months</li>
+                            <li>Yoga area for friends who want to relax the body</li>
+                        </ol>
+                    </div>
+                        <div>
+                        <img alt="choose us image" />
+                        </div>
+                </div>
+        </section>
+    </>)
+}
+
+export default ChooseUs;
