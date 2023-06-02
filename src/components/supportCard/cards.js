81ccc5c461a04a8a840c61e3c74674fe28c36167
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-02 17:14:20 +0545

    Add and import iconType to array of  cards.js object icon

--- src/components/supportCard/cards.js
+++ src/components/supportCard/cards.js
@@ -1,27 +1,30 @@
+import {BiCoffee, BiSmile, BiHeart} from "react-icons/bi";
+import {FiFeather} from "react-icons/fi"
+
 const cards = [
     {
         id: 1,
         title: "Free food and drinking during training",
         description: "We provide a variety of vitamin rich foods and snack for you during your training",
-        icon : ""
+        icon : BiCoffee
     },
     {
         id: 2,
         title: "Free to choose fitness trainer",
         description: "You can determine which trainer you want during the fitness tutorial, as well as trainer who is ready to guide you",
-        icon : ""
+        icon : BiSmile
     },
     {
         id: 3,
         title: "Fast service when you practice",
         description: "We have service with premium quality when you have subscribed with a minimum duration of 3 months",
-        icon : ""
+        icon : BiHeart
     },
     {
         id: 4,
         title: "Cool and clean rest room",
         description: "We always pay attention to quality of comfort and clean rest room inorder to make members feel comfortable and at home",
-        icon : ""
+        icon : FiFeather
     }
 ]
 
