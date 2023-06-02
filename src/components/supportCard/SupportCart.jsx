f768e7ac18c85a012514c8f267b90a27412dfdb8
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-02 14:09:40 +0545

    Refactor props

--- src/components/supportCard/SupportCart.jsx
+++ src/components/supportCard/SupportCart.jsx
@@ -1,15 +1,15 @@
 import Card from 'react-bootstrap/Card';
 // import Col from 'react-bootstrap/esm/Col';
 
-function SupportCart(props){
+function SupportCart({title, description}){
     return(
         <>
           <Card style={{ width: '16rem' }} className='card col-lg-4 col-md-3'>
       <Card.Body>
       {/* <i></i> */}
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <Card.Text>
-        {props.description}
+        {description}
         </Card.Text>
         <span>Read more</span>
       </Card.Body>
