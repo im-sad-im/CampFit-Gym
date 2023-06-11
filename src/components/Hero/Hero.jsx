bb6ce07de4832318b7579f3f6c4f9f57ec856076
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-11 14:04:24 +0545

    Change hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,8 @@
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import HeroImg from "../../assets/img/hero/heroImgg.png"
+// import HeroImg from "../../assets/img/hero/heroImgg.png"
+import HeroImg from "../../assets/img/hero/mainHero.png"
 import  Button  from "react-bootstrap/Button"
 import {BsFillPlayCircleFill} from "react-icons/bs";
 
@@ -12,16 +13,16 @@ function Hero(){
             <div id="hero" className="hero-section">
             <Container>
             <Row>
-                <Col lg={8}>
+                <Col lg={6}>
                 <h1 className='text-uppercase font-head'>build your body into a <span className="primary-text-color">healthy</span> and <span className="primary-text-color">strong body</span>.</h1>
-                <p>Sport is part of heath, so be diligent in exercising so that the body becomes strongerr and healthier to improve health and keep away from injury</p>
+                <p className="subtitle">Sport is part of heath, so be diligent in exercising so that the body becomes strongerr and healthier to improve health and keep away from injury</p>
                 <div className="d-flex gap-1">
                 <Button className="btn-main my-btn">Join Member</Button>
                 <Button variant="dark" className="my-btn my-btn1" style={{backgroundColor : "transparent", border: "none"}}> 
                  Watch Training <BsFillPlayCircleFill className="play-icon" style={{fontSize: "1.25rem"}} /></Button>
                 </div>
                 </Col>
-                <Col lg={4} className="text-center">
+                <Col lg={6} className="text-center">
                     <img src={HeroImg} alt="hero-img" />
                 </Col>
             </Row>
