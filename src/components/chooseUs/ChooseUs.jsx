4d627f913fd9cf34571586fa99c7daf08c49f170
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-12 15:06:26 +0545

    text center

--- src/components/chooseUs/ChooseUs.jsx
+++ src/components/chooseUs/ChooseUs.jsx
@@ -4,32 +4,32 @@ import chooseImage from "../../assets/img/choose-us/choose.png"
 import Col from "react-bootstrap/esm/Col";
 
 
-function ChooseUs(){
-    return(
+function ChooseUs() {
+    return (
         <section id="chooseUs" className="choose-us ebook-img-wrapper">
-        <div className="container">
-        <Row>
-        <Col lg={6}>
-                    <div className="choose-us-content">
-                        <h2 className="text-uppercase font-head sub-heading">what makes you <span className="sub-heading primary-text-color font-head">sure</span> to choose us?</h2>
-                        <ol>
-                            <li>Campfit provides free boxing arena and pool facitlites for those who have premium members</li>
-                            <li>There is a cold and comfortable resting rooom for members</li>
-                            <li>Free overseas vacation tickets for members who subscribe for more than 6 months</li>
-                            <li>Yoga area for friends who want to relax the body</li>
-                        </ol>
-                    </div>
-        </Col>
-        <Col lg={6}>
-                        <div>
-                        <img  src={chooseImage} alt="choose us image" />
+            <div className="container">
+                <Row>
+                    <Col lg={6}>
+                        <div className="choose-us-content">
+                            <h2 className="text-uppercase font-head sub-heading">what makes you <span className="sub-heading primary-text-color font-head">sure</span> to choose us?</h2>
+                            <ol>
+                                <li>Campfit provides free boxing arena and pool facitlites for those who have premium members</li>
+                                <li>There is a cold and comfortable resting rooom for members</li>
+                                <li>Free overseas vacation tickets for members who subscribe for more than 6 months</li>
+                                <li>Yoga area for friends who want to relax the body</li>
+                            </ol>
                         </div>
-        </Col>
-        </Row>
-        </div>
-        
+                    </Col>
+                    <Col lg={6}>
+                        <div className="text-center">
+                            <img src={chooseImage} alt="choose us image" />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+
         </section>
-)
+    )
 }
 
 export default ChooseUs;
