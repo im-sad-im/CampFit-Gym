9ba4a822245aafa708f0b61bab901a5ae3f2b34a
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-13 19:41:36 +0545

    Add testimo component

--- src/components/testimonials/Testimo.jsx
+++ src/components/testimonials/Testimo.jsx
@@ -1,8 +1,22 @@
+import image from "../../assets/img/testimonial/test.png"
+
 export default function Testimo(){
     return(
         <section id="testimo">
-            <div className="container">
 
+            <div className="container">
+            <h1>testimonials</h1>
+                <div className="d-flex flex-column">
+                    <p>What do they say?</p>
+                    <p>
+                        "Thank you Campfit for begin my mentor during training, and thank you for making my body ideal and strong, thank you once again for the world tournament that has put your trust in me :D"
+                    </p>
+                    <span>Selena Margaretha</span>
+                    <span>2022</span>
+                </div>
+                <div>
+                    <img src={image} />
+                </div>
             </div>
         </section>
     );
