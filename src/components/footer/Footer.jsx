15d0b3e8f49085073d7b1e03490d0b95cd24c08a
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-14 16:09:56 +0545

    Div

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 export default function Footer(){
     return(
-        <div className="container text-center">
+        <div className="container text-center footer">
+        <div className="footer-line"></div>
             <span> © 2021-2022, All Rights Reserved</span>
         </div>
     )
