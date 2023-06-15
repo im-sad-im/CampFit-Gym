b2f6463af4a5260fcef84ef993ca20d2c39f5e41
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-15 14:25:55 +0545

    Feat: preventDefault submit behavior

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,6 +1,6 @@
 export default function ContactForm(){
     return(
-        <form className="d-flex flex-column gap-2" style={{maxWidth:"80%"}}>
+        <form className="d-flex flex-column gap-2" style={{maxWidth:"80%"}} onSubmit={(e)=>{e.preventDefault();}}>
             <input type="text" name="name" placeholder="Name" className="rounded p-2 inputField"/>
             <input type="email" name="email" id="email" placeholder="Email" className="rounded p-2 inputField"/>
             <textarea name="message" rows="4" placeholder="Message" className="rounded p-2 inputField" style={{resize: "none"}}/>
