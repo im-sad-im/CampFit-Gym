6f7864462cabbd02ef27e57eaff90a96b0c05d31
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-15 13:52:19 +0545

    Add currentDate footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
 export default function Footer(){
+    
+    //store value in currentDate returned by a date object
+    const currentDate = new Date().getFullYear()
+
     return(
         <div className="container text-center footer">
         <div className="footer-line"></div>
-            <span> © 2021-2022, All Rights Reserved</span>
+            <span> © 2021-{currentDate}, All Rights Reserved</span>
         </div>
     )
 }
