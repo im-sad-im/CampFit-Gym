3ef3503b4fb1db421ada196cda0bbc954d252dbe
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-17 13:13:08 +0545

    Add Achievement component

---
+++ src/components/champion/Achievement.jsx
@@ -0,0 +1,10 @@
+export default function Achievement(){
+    return(
+        <div>
+                <div className="w-50">
+                            <h2 className="font-head sub-heading">37<span className="primary-text-color font-head">K</span></h2>
+                            <p className="subtitle sub-sub-font">World Champions</p>
+                        </div>
+        </div>
+    );
+}
