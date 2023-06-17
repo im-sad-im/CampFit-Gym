e8ae1cbd04df8a003ef030aa10cd739701380ad3
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-17 13:14:21 +0545

    Parameter props

--- src/components/champion/Achievement.jsx
+++ src/components/champion/Achievement.jsx
@@ -1,9 +1,9 @@
-export default function Achievement(){
+export default function Achievement({number, title}){
     return(
         <div>
                 <div className="w-50">
-                            <h2 className="font-head sub-heading">37<span className="primary-text-color font-head">K</span></h2>
-                            <p className="subtitle sub-sub-font">World Champions</p>
+                            <h2 className="font-head sub-heading">{number}<span className="primary-text-color font-head">K</span></h2>
+                            <p className="subtitle sub-sub-font">{title}</p>
                         </div>
         </div>
     );
