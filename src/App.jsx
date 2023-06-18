e5bb28abe56296a819f1d966bf28b6f561e6f8d6
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-18 17:30:57 +0545

    Routes Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+
 import NavbarNav from "./components/Nav/NavbarNav"
 import Home from "./pages/Home"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <NavbarNav />
-      <Home />
+      <Routes>
+            <Route path="/" element={<Home />} />
+      </Routes>
 
     </>
   )
