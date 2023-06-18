4f7b6952fa2c23e19a80c9092ed06c1181b9eeea
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-18 17:40:28 +0545

    Route to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 import NavbarNav from "./components/Nav/NavbarNav"
-import Home from "./pages/Home"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { Route,Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Service from "./pages/Service"
+import Gallery from "./pages/Gallery";
+import Community from "./pages/Community";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <NavbarNav />
       <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/community" element={<Community />} />
       </Routes>
 
     </>
