9a2cb1373dbd04a1d5bed69f2f5951b507f19668
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-19 13:51:38 +0545

    feat : triggers the event (countUp animation)  on scroll

--- src/components/champion/Achievement.jsx
+++ src/components/champion/Achievement.jsx
@@ -1,14 +1,19 @@
 import CountUp from "react-countup"
+import { useState } from "react";
+import ScrollTrigger from "react-scroll-trigger"
 
 export default function Achievement({number, title}){
+    const [isScrollTrigger, setIsScrollTrigger] = useState(false);
+
     return(
-        <div>
+        <ScrollTrigger onEnter={()=>{setIsScrollTrigger(true)}} onExit={()=>{setIsScrollTrigger(false)}}>
                 <div className="w-50">
-                {/* starts a count up transition on render */}
-                    <CountUp duration={2} delay={0} end={number} className="font-head sub-heading" />
+                {/* starts a count up transition on render when scroll triggers the event*/}
+                {isScrollTrigger && <CountUp duration={2} delay={0} end={number} className="font-head sub-heading" />}  
                            <span className="primary-text-color font-head sub-heading">K</span>
                             <p className="subtitle sub-sub-font">{title}</p>
                         </div>
-        </div>
+        </ScrollTrigger>
+       
     );
 }
