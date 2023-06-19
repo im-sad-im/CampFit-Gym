f349b5db66b88f56dcb7db01c6a42e0bbe930e4f
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-19 13:42:42 +0545

    feat: added countUP that starts a countup transition on render

--- src/components/champion/Achievement.jsx
+++ src/components/champion/Achievement.jsx
@@ -1,8 +1,12 @@
+import CountUp from "react-countup"
+
 export default function Achievement({number, title}){
     return(
         <div>
                 <div className="w-50">
-                            <h2 className="font-head sub-heading">{number}<span className="primary-text-color font-head">K</span></h2>
+                {/* starts a count up transition on render */}
+                    <CountUp duration={2} delay={0} end={number} className="font-head sub-heading" />
+                           <span className="primary-text-color font-head sub-heading">K</span>
                             <p className="subtitle sub-sub-font">{title}</p>
                         </div>
         </div>
