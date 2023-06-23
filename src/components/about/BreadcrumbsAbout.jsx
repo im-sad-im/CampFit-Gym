2ff6e20ca6fbe569162e3cb550d38b3128f0292c
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-23 17:28:52 +0545

    Add Breadcrumb, home icon

--- src/components/about/BreadcrumbsAbout.jsx
+++ src/components/about/BreadcrumbsAbout.jsx
@@ -1,7 +1,24 @@
-export default function BreadcrumbsAbout(){
-    return(
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import {FaHome} from "react-icons/fa";
+
+export default function BreadcrumbsAbout() {
+    return (
         <section className="breadcrumbs-about">
-            {/* <h1>About us</h1> */}
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>About Us</h2>
+                        <Breadcrumb>
+                            <Breadcrumb.Item href="/" > <FaHome className="home-icon"/> Home</Breadcrumb.Item>
+                            <Breadcrumb.Item active>About</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Col>
+                </Row>
+            </Container>
+
         </section>
     );
 }
