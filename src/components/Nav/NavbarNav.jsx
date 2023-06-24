d535b85648d7e710e8e740b1690ec7d3b672553c
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-24 17:20:56 +0545

    Fix :  router is not picking up route changes

--- src/components/Nav/NavbarNav.jsx
+++ src/components/Nav/NavbarNav.jsx
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logoImg from '../../assets/img/logo/campfitLogo.png'
+import {Link} from "react-router-dom";
 
 function NavbarNav() {
   return (
     <Navbar variant='dark' expand="lg" fixed='top' style={{backgroundColor: "#000"}} >
       <Container>
-        <Navbar.Brand href="/"><img src={logoImg} alt="campfit-logo" style={{width: "4rem"}}/></Navbar.Brand>
+        <Navbar.Brand as={Link} to="/"><img src={logoImg} alt="campfit-logo" style={{width: "4rem"}}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/service">Service</Nav.Link>
-            <Nav.Link href="/gallery">Gallery</Nav.Link>
-            <Nav.Link href="/community">Community</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/service">Service</Nav.Link>
+            <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+            <Nav.Link as={Link} to="/community">Community</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
