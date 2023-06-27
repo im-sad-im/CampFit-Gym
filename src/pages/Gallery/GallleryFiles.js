47799b16c6bc82183d6b3f4a79a8d8c211f71802
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-27 12:02:14 +0545

    feat : added gallery image files

---
+++ src/pages/Gallery/GallleryFiles.js
@@ -0,0 +1,74 @@
+import Img1 from "../../assets/img/gallery/1.jpg"
+import Img2 from "../../assets/img/gallery/2.jpg"
+import Img3 from "../../assets/img/gallery/3.jpg"
+import Img4 from "../../assets/img/gallery/4.jpg"
+import Img5 from "../../assets/img/gallery/5.jpg"
+import Img6 from "../../assets/img/gallery/6.jpg"
+import Img7 from "../../assets/img/gallery/7.jpg"
+import Img8 from "../../assets/img/gallery/8.jpg"
+import Img9 from "../../assets/img/gallery/9.jpg"
+import Img10 from "../../assets/img/gallery/10.jpg"
+import Img11 from "../../assets/img/gallery/11.jpg"
+import Img12 from "../../assets/img/gallery/12.jpg"
+import Img13 from "../../assets/img/gallery/13.jpg"
+import Img14 from "../../assets/img/gallery/14.jpg"
+
+
+export const galleryImg = [
+    {
+        img: Img1,
+        id:1
+    },
+    {
+        img: Img2,
+        id:2
+    },
+    {
+        img: Img3,
+        id:3
+    },
+    {
+        img: Img4,
+        id:4
+    },
+    {
+        img: Img5,
+        id:5
+    },
+    {
+        img: Img6,
+        id:6
+    },
+    {
+        img: Img7,
+        id:7
+    },
+    {
+        img: Img8,
+        id:8
+    },
+    {
+        img: Img9,
+        id:9
+    },
+    {
+        img: Img10,
+        id:10
+    },
+    {
+        img: Img11,
+        id:11
+    },
+    {
+        img: Img12,
+        id:12
+    },
+    {
+        img: Img13,
+        id:13
+    },
+    {
+        img: Img14,
+        id:14
+    }
+];
