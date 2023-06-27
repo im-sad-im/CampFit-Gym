f6c2f46d551429443988a102744378a7b2e3140c
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-27 09:21:25 +0545

    Update gallery

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,12 +1,14 @@
 import BreadcrumbsBanner from "../../components/about/BreadcrumbsBanner";
+import Footer from "../../components/footer/Footer";
 
 export default function Gallery(){
     return(
         <section className="gallery">
-        <BreadcrumbsBanner />
+        <BreadcrumbsBanner heading={"Gallery"} currentPage={"Gallery"}/>
         <div className="container">
             <h1>Gallery page</h1>
         </div>
+        <Footer />
         </section>
     )
 }
