e398d76d7c0f4732175c92dcf61744ffd5c4e981
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-27 18:24:34 +0545

    Update Gallery pages

--- src/pages/Gallery/GalleryPage1.jsx
+++ src/pages/Gallery/GalleryPage1.jsx
@@ -13,7 +13,7 @@ export default function GalleryPage1() {
                 <Row>
                     {
                         firstPage1.map((image) => {
-                            return (<Col lg={4} md={6} key={image.id}> <img src={image.img} alt="gallery-img" className="mw-100 height-auto my-2"/></Col>);
+                            return (<Col lg={4} md={6} key={image.id}> <img src={image.img} alt="gallery-img" className="mw-100 height-auto my-2" /></Col>);
                         })
                     }
                 </Row>
@@ -21,9 +21,9 @@ export default function GalleryPage1() {
             <div className="d-flex justify-content-center mt-5">
                 <Pagination size="lg">
                     {/* <Pagination.Prev /> */}
-                    <Pagination.Item linkClassName="page-Item">{1}</Pagination.Item>
+                    <Pagination.Item>{1}</Pagination.Item>
                     <Pagination.Item href="page2">{2}</Pagination.Item>
-                    <Pagination.Next href="page2"/>
+                    <Pagination.Next href="page2" />
                 </Pagination>
             </div>
         </>
--- src/pages/Gallery/GalleryPage2.jsx
+++ src/pages/Gallery/GalleryPage2.jsx
@@ -8,17 +8,17 @@ export default function GalleryPage2() {
     const secondPage2 = galleryImg.filter((item) => { return (item.id > 8) });
     return (
         <>
-        <Container>
-            <Row>
-                {
-                    secondPage2.map((image) => {
-                        return (<Col lg={4} md={6} key={image.id}> <img src={image.img} alt="gallery-img" className="mw-100  my-2" /></Col>);
-                    })
-                }
-            </Row>
-        </Container>
+            <Container>
+                <Row>
+                    {
+                        secondPage2.map((image) => {
+                            return (<Col lg={4} md={6} key={image.id}> <img src={image.img} alt="gallery-img" className="mw-100  my-2" /></Col>);
+                        })
+                    }
+                </Row>
+            </Container>
 
-        <div className="d-flex justify-content-center mt-5">
+            <div className="d-flex justify-content-center mt-5">
                 <Pagination size="lg">
                     <Pagination.Prev href="page1" />
                     <Pagination.Item href="page1">{1}</Pagination.Item>
