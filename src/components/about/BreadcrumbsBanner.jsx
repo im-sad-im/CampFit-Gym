7cc889d3fdb1f1d4042a5e9760cdcf00349d3498
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-27 08:53:50 +0545

    Added props to BreadcrumbsBanner

--- src/components/about/BreadcrumbsBanner.jsx
+++ src/components/about/BreadcrumbsBanner.jsx
@@ -4,16 +4,16 @@ import Col from "react-bootstrap/esm/Col";
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import {FaHome} from "react-icons/fa";
 
-export default function BreadcrumbsAbout() {
+export default function BreadcrumbsBanner({heading, currentPage}) {
     return (
         <section className="breadcrumbs-about set-bg">
             <Container>
                 <Row>
                     <Col>
-                        <h2>About Us</h2>
+                        <h2>{heading}</h2>
                         <Breadcrumb>
                             <Breadcrumb.Item href="/" > <FaHome className="home-icon"/> Home</Breadcrumb.Item>
-                            <Breadcrumb.Item active>About</Breadcrumb.Item>
+                            <Breadcrumb.Item active>{currentPage}</Breadcrumb.Item>
                         </Breadcrumb>
                     </Col>
                 </Row>
