df4b465cd85fa626392451b90b161e025d3ab4ab
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-30 09:26:29 +0545

    Added nested route for Schedule

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Service from "./pages/Service"
 import Gallery from "./pages/Gallery/Gallery";
-import Schedule from "./pages/Schedule/Schedule";
 import GalleryPage1 from "./pages/Gallery/GalleryPage1";
 import GalleryPage2 from "./pages/Gallery/GalleryPage2";
+
+import Schedule from "./pages/Schedule/Schedule";
+import Monday from "./pages/Schedule/Monday";
+import Tuesday from "./pages/Schedule/Tuesday";
+import Wednesday from "./pages/Schedule/Wednesday";
+import Thursday from "./pages/Schedule/Thursday";
+import Friday from "./pages/Schedule/Friday";
+import Saturday from "./pages/Schedule/Saturday";
+import Sunday from "./pages/Schedule/Sunday";
 
 import { Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Monday from "./pages/Schedule/Monday";
 function App() {
   return (
     <>
@@ -22,12 +29,18 @@ function App() {
         <Route path="/about" element={<About />} />
 
         <Route path="/gallery" element={<Gallery />} >
-           <Route path="page1" element={<GalleryPage1 />} /> 
+          <Route path="page1" element={<GalleryPage1 />} />
           <Route path="page2" element={<GalleryPage2 />} />
         </Route>
 
         <Route path="/schedule" element={<Schedule />}>
           <Route path="monday" element={<Monday />} />
+          <Route path="tuesday" element={<Tuesday />} />
+          <Route path="wednesday" element={<Wednesday />} />
+          <Route path="thursday" element={<Thursday />} />
+          <Route path="friday" element={<Friday />} />
+          <Route path="saturday" element={<Saturday />} />
+          <Route path="sunday" element={<Sunday />} />
         </Route>
       </Routes>
 
