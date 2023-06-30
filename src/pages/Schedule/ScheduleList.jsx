7e489fccf3e37afbad4839ffb76d0f0d91215f55
Sadim Mali <sadimmalakar77@gmail.com>
2023-06-30 10:14:08 +0545

    MOdify components

--- src/pages/Schedule/ScheduleList.jsx
+++ src/pages/Schedule/ScheduleList.jsx
@@ -1,17 +1,20 @@
-export default function ScheduleList(){
-    return(
-        <ul className="d-flex justify-content-between list-unstyled">
-            <li>
+export default function ScheduleList({classType, classTime, trainerName}) {
+    return (
+        <ul className="d-flex justify-content-between list-unstyled w-100" style={{backgroundColor: "#211F1F"}}>
+            <li className="w-100 text-center py-4">
                 <p className="subtitle">class Name</p>
                 <p> {classType} </p>
             </li>
-            <li>
-            <p className="subtitle">Time</p>
-            <p> {classTime} </p>
+            <li className="w-100 text-center py-4">
+                <p className="subtitle">Time</p>
+                <p> {classTime} </p>
             </li>
-            <li>
+            <li className="w-100 text-center py-4">
                 <p className="subtitle">Trainer</p>
                 <p> {trainerName} </p>
+            </li>
+            <li className="w-100 text-center py-5">
+                <button className="btn-main my-btn px-5"> Join us</button>
             </li>
         </ul>
     )
