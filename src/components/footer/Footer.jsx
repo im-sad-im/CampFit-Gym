d6e1844353d1d94886105f34afce1961d8cbf0bb
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-01 09:14:55 +0545

    Refactor : Footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,16 @@
+import Container from "react-bootstrap/esm/Container"
+
 export default function Footer(){
     
     //store value in currentDate returned by a date object
     const currentDate = new Date().getFullYear()
 
     return(
-        <footer className="container text-center footer">
+        <footer className="p-4">
+        <Container className="text-center">
         <div className="footer-line"></div>
             <span> © 2021-{currentDate}, All Rights Reserved</span>
+        </Container>
         </footer>
     )
 }
