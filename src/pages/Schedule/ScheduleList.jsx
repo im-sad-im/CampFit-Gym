d070d27d8d183951143cefafda6b3690087e6df8
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-01 17:33:04 +0545

    style : responsive css

--- src/pages/Schedule/ScheduleList.jsx
+++ src/pages/Schedule/ScheduleList.jsx
@@ -1,6 +1,6 @@
 export default function ScheduleList({classType, classTime, trainerName}) {
     return (
-        <ul className="d-flex justify-content-between list-unstyled w-100" style={{backgroundColor: "#211F1F"}}>
+        <ul className="d-flex justify-content-between list-unstyled w-100 schedule-list" style={{backgroundColor: "#211F1F"}}>
             <li className="w-100 text-center py-4">
                 <p className="subtitle">class Name</p>
                 <p> {classType} </p>
