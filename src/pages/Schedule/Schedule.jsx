018c4d2526af2258f4571491244a92df213e2490
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-01 09:28:39 +0545

    Modify schedule component

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -11,33 +11,31 @@ export default function Schedule() {
     return (
         <section id="schedule">
             <BreadcrumbsBanner heading={"Schedule"} currentPage={"schedule"} />
-            <Container className="py-5">
-                <Row>
+            <Container className="my-5">
                 <h1 className="font-head sub-sub-heading text-center">Schedule by </h1>
-                    <Col lg={12}>
-                        <Link to='/schedule/monday'>
-                            <Button className="btn-main my-btn">Monday</Button>
-                        </Link>
-                        <Link to="/schedule/tuesday">
-                            <Button className="btn-main my-btn">Tuesday</Button>
-                        </Link>
-                        <Link to='/schedule/wednesday'>
-                            <Button className="btn-main my-btn">Wednesday</Button>
-                        </Link>
-                        <Link to='/schedule/thursday'>
-                            <Button className="btn-main my-btn">Thursday</Button>
-                        </Link>
-                        <Link to='/schedule/friday'>
-                            <Button className="btn-main my-btn">Friday</Button>
-                        </Link>
-                        <Link to='/schedule/saturday'>
-                            <Button className="btn-main my-btn">Saturday</Button>
-                        </Link>
-                        <Link to='/schedule/sunday'>
-                            <Button className="btn-main my-btn">Sunday</Button>
-                        </Link>
-                    </Col>
-                </Row>
+                <div className="d-flex gap-2 flex-wrap justify-content-center"> 
+                    <Link to='/schedule/monday'>
+                        <Button className="btn-main my-btn">Monday</Button>
+                    </Link>
+                    <Link to="/schedule/tuesday">
+                        <Button className="btn-main my-btn">Tuesday</Button>
+                    </Link>
+                    <Link to='/schedule/wednesday'>
+                        <Button className="btn-main my-btn">Wednesday</Button>
+                    </Link>
+                    <Link to='/schedule/thursday'>
+                        <Button className="btn-main my-btn">Thursday</Button>
+                    </Link>
+                    <Link to='/schedule/friday'>
+                        <Button className="btn-main my-btn">Friday</Button>
+                    </Link>
+                    <Link to='/schedule/saturday'>
+                        <Button className="btn-main my-btn">Saturday</Button>
+                    </Link>
+                    <Link to='/schedule/sunday'>
+                        <Button className="btn-main my-btn">Sunday</Button>
+                    </Link>
+                </div>
             </Container>
             <Outlet />
             <Contact />
