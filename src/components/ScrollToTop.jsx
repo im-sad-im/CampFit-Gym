1276f99534e9297399362b5a9fb3a0b68ba8cc9f
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-03 16:41:49 +0545

    Update Scroll to top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { AiFillCaretUp } from "react-icons/ai";
 import { IconContext } from "react-icons/lib";
+
 export default function ScrollToTop() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    
+    const toggleVisible = () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+            setIsVisible(true);
+        else
+            setIsVisible(false);
+    }
+
+    //Triggers toggleVisible function when scroll event is triggered
+    window.addEventListener('scroll', toggleVisible);
+
+    const goToTop = () => {
+        window.scrollTo({ top: 0 });
+    }
     return (
         <>
-            <IconContext.Provider value={{ className: "scroll-btn", color: "#000" }}>
-                <AiFillCaretUp size={50} />
-            </IconContext.Provider>
+            {/* Renders button if isVisible is true */}
+            {
+                isVisible && (<IconContext.Provider value={{ className: "scroll-btn", color: "#000" }}>
+                    <AiFillCaretUp size={50} onClick={goToTop} />
+                </IconContext.Provider>)
+            }
         </>
     );
 }
